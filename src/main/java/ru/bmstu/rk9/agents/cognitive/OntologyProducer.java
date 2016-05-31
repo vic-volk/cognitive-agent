61f4cee8b3484fb3b944b8de1ede3d77bd602f03
Andrew <vic-volk@yandex.ru>
2016-06-01 00:19:16 +0300

    intermediate;

--- src/main/java/ru/bmstu/rk9/agents/cognitive/OntologyProducer.java
+++ src/main/java/ru/bmstu/rk9/agents/cognitive/OntologyProducer.java
@@ -1,10 +1,11 @@
 package ru.bmstu.rk9.agents.cognitive;
 
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 import java.util.List;
 
 public interface OntologyProducer {
 
-    OWLOntology create(List<String> sentences);
+    OWLOntology create(List<String> sentences) throws OWLOntologyCreationException;
 }
--- src/main/java/ru/bmstu/rk9/agents/cognitive/hard/HardOntologyProducer.java
+++ src/main/java/ru/bmstu/rk9/agents/cognitive/hard/HardOntologyProducer.java
@@ -1,14 +1,35 @@
 package ru.bmstu.rk9.agents.cognitive.hard;
 
-import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.util.DefaultPrefixManager;
 import ru.bmstu.rk9.agents.cognitive.OntologyProducer;
 
 import java.util.List;
 
 public class HardOntologyProducer implements OntologyProducer {
 
+    private final String defaultPrefix = "#bookOntology";
+
     @Override
-    public OWLOntology create(List<String> sentences) {
-        return null;
+    public OWLOntology create(List<String> sentences) throws OWLOntologyCreationException {
+        List<String> preparedSentences = prepare(sentences);
+        OWLDataFactory dataFactory = OWLManager.getOWLDataFactory();
+        OWLOntologyManager ontologyManager = OWLManager.createOWLOntologyManager();
+
+        IRI iri = IRI.create(defaultPrefix);
+        OWLOntology o = ontologyManager.createOntology(iri);
+        OWLClass book = dataFactory.getOWLClass(IRI.create(defaultPrefix, "#book"));
+        OWLClass metaInformation = dataFactory.getOWLClass(IRI.create(defaultPrefix, "#meta-information"));
+
+        OWLObjectProperty
+        OWLIndividual concreteBook = dataFactory.getOWLNamedIndividual();
+        //TODO: process book names
+        //TODO: process dates
+        return preparedSentences;
+    }
+
+    private List<String> prepare(List<String> sentences) {
+        return sentences;
     }
 }
